16ad995a262d00c5514d543f2151f2ddb51b128b
TonyTech <toomgbebu@gmail.com>
2024-07-13 20:24:14 +0100

    progress and responsiveness of header section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+"use client";
+
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const links = [
   { url: "/", title: "Home" },
@@ -7,11 +11,32 @@ const links = [
   { url: "/contact", title: "Contact" },
 ];
 
+import Github from "../../public/github.png";
+import Dribble from "../../public/dribbble.png";
+import Instagram from "../../public/instagram.png";
+import Facebook from "../../public/facebook.png";
+import Pinterest from "../../public/pinterest.png";
+import LinkedIn from "../../public/linkedin.png";
+import NavLinks from "./NavLinks";
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 first-letter:md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 first-letter:md:px-12 lg:px-20 xl:px-48 text-lg">
+      {/* LINKS... */}
+
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          //   <Link href={link.url} key={link.title}>
+          //     {link.title}
+          //   </Link>
+
+          <NavLinks link={link} key={link.title} />
+        ))}
+      </div>
+
       {/* LOGO */}
-      <div className="">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -23,11 +48,37 @@ const Navbar = () => {
         </Link>
       </div>
 
+      {/* SOCIAL MEDIA LINKS.... */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="#">
+          <Image src={Github} alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src={Dribble} alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src={Instagram} alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src={Facebook} alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src={Pinterest} alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src={LinkedIn} alt="" width={24} height={24} />
+        </Link>
+      </div>
+
       {/* RESPONSIVE MENU */}
-      <div className="">
+
+      <div className="md:hidden">
         {/* MENU BUTTON */}
 
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
@@ -35,13 +86,15 @@ const Navbar = () => {
 
         {/* MENU LIST */}
 
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-          {links.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
